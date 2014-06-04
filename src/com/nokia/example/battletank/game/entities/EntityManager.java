5816bfa9957402e895e8cba714c29c994dacabb7
Tomi Paananen <tomi.paananen@nokia.com>
2014-06-04 12:32:07 +0300

    Updated the copyright headings.

--- src/com/nokia/example/battletank/game/entities/EntityManager.java
+++ src/com/nokia/example/battletank/game/entities/EntityManager.java
@@ -1,11 +1,8 @@
-/**
-* Copyright (c) 2012-2014 Microsoft Mobile. All rights reserved.
-* Nokia and Nokia Connecting People are registered trademarks of Nokia Corporation. 
-* Oracle and Java are trademarks or registered trademarks of Oracle and/or its
-* affiliates. Other product and company names mentioned herein may be trademarks
-* or trade names of their respective owners. 
-* See LICENSE.TXT for license information.
-*/
+/*
+ * Copyright (c) 2012-2014 Microsoft Mobile. All rights reserved.
+ * Please see the license file delivered with this project for more details.
+ */
+
 package com.nokia.example.battletank.game.entities;
 
 import java.io.DataInputStream;
